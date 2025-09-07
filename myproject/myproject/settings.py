dd680f988d8a9e04d9bf6134ea222dc6433d2cc7
Ely J Jaime <ejaimech17@gmail.com>
2025-09-06 18:09:16 -0700

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5=0%@h$g^^vp+&mcalo@65j+u-qv=@7rcz3i$nun*21@c^1eso'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
